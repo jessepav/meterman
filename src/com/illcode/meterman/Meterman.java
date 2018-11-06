253c345398393e921ec9f0db551b33f48701e8b7
Jesse Pavel <jpavel@alum.mit.edu>
2018-11-06 16:08:10 -0500

    Fleshing out prefs and Utils

--- src/com/illcode/meterman/Meterman.java
+++ src/com/illcode/meterman/Meterman.java
@@ -1,8 +1,59 @@
 package com.illcode.meterman;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import static com.illcode.meterman.Utils.logger;
+
 public class Meterman
 {
-    public static void main(String[] args) {
+    static Path prefsPath, savesPath;
+    static Properties prefs;
 
+    public static void main(String[] args) throws IOException {
+        prefsPath = Paths.get("config/meterman.properties");
+        if (!loadPrefs(prefsPath)) {
+            System.err.println("Error loading prefs from " + prefsPath.toString());
+            System.exit(1);
+        }
+
+        Utils.initLogging();
+
+        savesPath = Paths.get(Utils.pref("saves-path", "saves"));
+        if (Files.notExists(savesPath))
+            Files.createDirectories(savesPath);
+    }
+
+    private static boolean loadPrefs(Path path) {
+        prefs = new Properties();
+        if (Files.exists(path)) {
+            try (FileReader r = new FileReader(path.toFile())) {
+                prefs.load(r);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void savePrefs(Path path) {
+        try (FileWriter w = new FileWriter(path.toFile())) {
+            prefs.store(w, "Meterman preferences");
+        } catch (IOException ex) {
+            logger.log(Level.WARNING, "savePrefs " + path.toString(), ex);
+        }
+    }
+
+    /** Called when the program is shutting down. */
+    public static void shutdown() {
+        logger.info("Meterman shutting down...");
+        savePrefs(prefsPath);
     }
 }
