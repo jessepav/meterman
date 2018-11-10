bb267bceb20248cf8f5d5a9700ceded93b6b1a79
Jesse Pavel <jpavel@alum.mit.edu>
2018-11-09 23:26:57 -0500

    Add turnBegin() to TurnListener

--- src/com/illcode/meterman/event/TurnListener.java
+++ src/com/illcode/meterman/event/TurnListener.java
@@ -5,6 +5,11 @@ package com.illcode.meterman.event;
  */
 public interface TurnListener
 {
-    /** Called at the end of each turn. */
+    /** Called at the beginning of each turn, before the player has moved or
+        performed an action, and before any other listeners are notified. */
+    void turnBegin();
+    
+    /** Called at the end of each turn, after the player has moved or
+        performed an action, and after all other listener types have been notified. */
     void turnEnd();
 }
