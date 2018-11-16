ee402d42a3d3d700dce329371c9bb3714ffa764e
Jesse Pavel <jpavel@alum.mit.edu>
2018-11-16 16:48:21 -0500

    Build and artifact updates

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0..
-java -Xmx256M -cp build\production\Meterman;lib\* -splash:assets/meterman-splash.png ^
+java -Xmx256M -cp build\production\Meterman;lib\* -splash:assets/meterman/splash-screen.png ^
     com.illcode.meterman.Meterman
 popd
