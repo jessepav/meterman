4d6a1a66af724a904e784ebb69d4d9c793d1ac44
Jesse Pavel <jpavel@alum.mit.edu>
2018-12-01 17:17:24 -0500

    Wrote the BasicWorldManager

--- src/com/illcode/meterman/impl/BasicWorldManager.java
+++ src/com/illcode/meterman/impl/BasicWorldManager.java
@@ -2,10 +2,12 @@ package com.illcode.meterman.impl;
 
 import com.illcode.meterman.Entity;
 import com.illcode.meterman.Game;
-import com.illcode.meterman.Meterman;
 import com.illcode.meterman.Utils;
-import com.illcode.meterman.event.EntityActionProcessor;
+import com.illcode.meterman.event.EntityActionsProcessor;
 import com.illcode.meterman.event.GameActionListener;
+
+import static com.illcode.meterman.Meterman.gm;
+import static com.illcode.meterman.Attributes.*;
 
 import java.util.List;
 
@@ -18,7 +20,7 @@ import java.util.List;
  *     <li>Equipping and unequipping equippables.</li>
  * </ul>
  */
-public class BasicWorldManager implements GameActionListener, EntityActionProcessor
+public class BasicWorldManager implements GameActionListener, EntityActionsProcessor
 {
     public static final String BASIC_WORLD_MANAGER_KEY = "com.illcode.meterman.impl.BasicWorldManager";
 
@@ -38,21 +40,68 @@ public class BasicWorldManager implements GameActionListener, EntityActionProces
      * This should be called in {@link Game#start(boolean)} when a new game is started.
      */
     public void install() {
+        gm.addDefaultGameActionListener(this);
+        gm.addEntityActionsProcessor(this);
+        gm.getWorldData().put(BASIC_WORLD_MANAGER_KEY, this);
+    }
 
-        Meterman.gm.getWorldData().put(BASIC_WORLD_MANAGER_KEY, this);
+    /**
+     * Removes this BasicWorldManager from the GameManager and world-data.
+     */
+    public void remove() {
+        gm.removeDefaultGameActionListener(this);
+        gm.removeEntityActionsProcessor(this);
+        gm.getWorldData().remove(BASIC_WORLD_MANAGER_KEY);
     }
 
     /** Retrieve the BasicWorldManager instance (saved by {@link #install()}) from the
      *  WorldState's worldData */
     public static BasicWorldManager getBasicWorldManager() {
-        return (BasicWorldManager) Meterman.gm.getWorldData().get(BASIC_WORLD_MANAGER_KEY);
+        return (BasicWorldManager) gm.getWorldData().get(BASIC_WORLD_MANAGER_KEY);
     }
 
     public void processEntityActions(Entity e, List<String> actions) {
-
+        if (e.checkAttribute(TAKEABLE)) {
+            if (gm.isInInventory(e))
+                actions.add(DROP_ACTION_NAME);
+            else
+                actions.add(TAKE_ACTION_NAME);
+        }
+        if (e.checkAttribute(WEARABLE) && gm.isInInventory(e)) {
+            if (gm.isWorn(e))
+                actions.add(TAKEOFF_ACTION_NAME);
+            else
+                actions.add(WEAR_ACTION_NAME);
+        }
+        if (e.checkAttribute(EQUIPPABLE) && gm.isInInventory(e)) {
+            if (gm.isEquipped(e))
+                actions.add(UNEQUIP_ACTION_NAME);
+            else
+                actions.add(EQUIP_ACTION_NAME);
+        }
     }
 
-    public boolean processAction(String action, Entity entity, boolean beforeAction) {
-        return false;
+    public boolean processAction(String action, Entity e, boolean beforeAction) {
+        if (action.equals(DROP_ACTION_NAME)) {
+            gm.moveEntity(e, e.getRoom());
+            return true;
+        } else if (action.equals(TAKE_ACTION_NAME)) {
+            gm.takeEntity(e);
+            return true;
+        } else if (action.equals(TAKEOFF_ACTION_NAME)) {
+            gm.setWorn(e, false);
+            return true;
+        } else if (action.equals(WEAR_ACTION_NAME)) {
+            gm.setWorn(e, true);
+            return true;
+        } else if (action.equals(UNEQUIP_ACTION_NAME)) {
+            gm.setEquipped(e, false);
+            return true;
+        } else if (action.equals(EQUIP_ACTION_NAME)) {
+            gm.setEquipped(e, true);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
