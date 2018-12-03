dbddc1a834f987f2a4920c8bbc0133d089fa6a87
Jesse Pavel <jpavel@alum.mit.edu>
2018-12-03 11:41:47 -0500

    Forgot to add TalkTopic.java

---
+++ src/com/illcode/meterman/impl/TalkTopic.java
@@ -0,0 +1,31 @@
+package com.illcode.meterman.impl;
+
+/**
+ * Represents a topic of discussion with an NPC
+ * <p/>
+ * NOTE: since <tt>TalkTopic</tt>s are always retrieved from a {@link WorldBuilder#loadTopicMap(String)
+ * topicMap}, and thus the same TalkTopic instance will be retrieved for a given key, we can compare and
+ * hash <tt>TalkTopic</tt>s by identity, and don't need to write <tt>hashCode</tt> and <tt>equals</tt>
+ * methods.
+ */
+public class TalkTopic
+{
+    /** Unique topic key, as found in the topic map */
+    String key;
+
+    /** Topic as shown to the user in the list dialog. There can be multiple <tt>TalkTopic</tt>s
+     *  with the same label, if the NPC will say different things depending on circumstances. */
+    String label;
+
+    /** What the NPC will say for this topic. */
+    String text;
+
+    public TalkTopic() {
+    }
+
+    public TalkTopic(String key, String label, String text) {
+        this.key = key;
+        this.label = label;
+        this.text = text;
+    }
+}
