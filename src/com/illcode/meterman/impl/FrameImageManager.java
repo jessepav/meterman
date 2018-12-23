cae9a6bdf4ed4c8c49cdeeaa9c22464748db7b75
Jesse Pavel <jpavel@alum.mit.edu>
2018-12-22 19:25:13 -0500

    Add FrameImageManager

---
+++ src/com/illcode/meterman/impl/FrameImageManager.java
@@ -0,0 +1,102 @@
+package com.illcode.meterman.impl;
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.ParseException;
+import com.illcode.meterman.Meterman;
+import com.illcode.meterman.Room;
+import com.illcode.meterman.event.PlayerMovementListener;
+import com.illcode.meterman.ui.UIConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+import static com.illcode.meterman.Utils.logger;
+
+public class FrameImageManager implements PlayerMovementListener
+{
+    private Map<String,String> roomImageMap;
+    private String defaultImageName;
+
+    public FrameImageManager() {
+    }
+
+    public void init() {
+        roomImageMap = new HashMap<>();
+        defaultImageName = UIConstants.DEFAULT_FRAME_IMAGE;
+    }
+
+    // TODO: write saveTo(), retrieveFrom(), register(), deregister()
+
+    public void setDefaultImageName(String defaultImageName) {
+        this.defaultImageName = defaultImageName;
+    }
+
+    public void setRoomImage(String roomId, String imageName) {
+        roomImageMap.put(roomId, imageName);
+    }
+
+    public String getRoomImage(String roomId) {
+        String name = roomImageMap.get(roomId);
+        return name != null ? name : defaultImageName;
+    }
+
+    public void clear() {
+        roomImageMap.clear();
+    }
+
+    public Map<String,String> getRoomImageMap() {
+        return roomImageMap;
+    }
+
+    public void setRoomImageMap(Map<String,String> roomImageMap) {
+        this.roomImageMap = roomImageMap;
+    }
+
+    /**
+     * Loads roomId->imageName mappings from JSON data of this form:
+     * <pre>{@code
+     * {
+     *     "default" : "defaultImageName",
+     *     "roomId1" : "imageName1",
+     *     "roomId2" : "imageName2",
+     *     ...
+     * }
+     * }</pre>
+     * <tt>default</tt> is a special "room ID" that is used if the ID of the current room is
+     * not specified explicitly in the mapping.
+     * <p/>
+     * This method replaces any mappings currently stored by the FrameImageManager.
+     * @param json JSON text
+     */
+    public void loadFromJson(String json) {
+        roomImageMap.clear();
+        try {
+            JsonObject o = Json.parse(json).asObject();
+            for (JsonObject.Member m : o) {
+                String roomId = m.getName();
+                String imageName = m.getValue().asString();
+                if (roomId.equals("default"))
+                    defaultImageName = imageName;
+                else
+                    roomImageMap.put(roomId, imageName);
+            }
+        } catch (ParseException | UnsupportedOperationException ex) {
+            logger.log(Level.WARNING, "JSON error, FrameImageManager.loadFromJson()", ex);
+        }
+
+    }
+
+    public boolean playerMove(Room from, Room to, boolean beforeMove) {
+        if (beforeMove == false) {
+            String imageName;
+            if (to instanceof BaseRoom)
+                imageName = getRoomImage(((BaseRoom) to).id);
+            else
+                imageName = defaultImageName;
+            Meterman.ui.setFrameImage(imageName);
+        }
+        return false;
+    }
+}
